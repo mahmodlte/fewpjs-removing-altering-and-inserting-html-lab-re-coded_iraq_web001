fdb7cdd2ae005ca8666f9852044f0d100e582185
Mahmoud Mansour <mahmodlte@gmail.com>
2020-05-15 11:39:36 +0300

    Lab completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let main = document.querySelector('main');
+main.remove();
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory')
+const head = newHeader.innerHTML;
+newHeader.innerHTML = "MAHMOUD-MANSOUR is the champion";
